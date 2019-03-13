982939c1984a3504da5298d323c5401e1767916f
sh1ark <48487918+sh1ark@users.noreply.github.com>
2019-03-13 11:14:30 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Special Thanks To Sami Zaidi !"
+read -p 'Press Enter To Start Installing ! '
+pkg install figlet
+apt install figlet
+apt install sqlmap
+pkg install sqlmap 
+mv shark /bin
+echo "Tool Installed  ! "
+shark
